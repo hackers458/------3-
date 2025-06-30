08db5f186ffa9b858fcb1eca8daefab49e994bcf
hackers458 <hackers458@naver.com>
2025-07-01 03:10:11 +0900

    Update board_view.php

--- board_view.php
+++ board_view.php
@@ -1,6 +1,9 @@
 
 
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once 'auth_check.php';
 ini_set('session.save_path', '/tmp');
 session_start();
