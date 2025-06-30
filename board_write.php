6b224d1e51938e47b618101ba796de4e2e39227b
hackers458 <hackers458@naver.com>
2025-07-01 01:27:29 +0900

    Update board_write.php

--- board_write.php
+++ board_write.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
-$conn = new mysqli("localhost", "root", "swjisj123!B", "user_info");
+$conn = new mysqli("kknock6.mysql.database.azure.com", "hackers458", "swjisj123!", "user_info");
 if ($conn->connect_error) {
     die("DB 연결 실패: " . $conn->connect_error);
 }
